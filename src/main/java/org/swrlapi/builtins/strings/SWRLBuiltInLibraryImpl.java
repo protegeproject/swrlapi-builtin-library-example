9aa1e225cf7e1a7a5d678e98772a7d7e851d6704
zippyrate <martin@zippyrate.com>
2017-04-26 08:21:21 -0700

    New built-in registration work

--- src/main/java/org/swrlapi/builtins/strings/SWRLBuiltInLibraryImpl.java
+++ src/main/java/org/swrlapi/builtins/strings/SWRLBuiltInLibraryImpl.java
@@ -1,14 +1,25 @@
 package org.swrlapi.builtins.strings;
 
 import org.swrlapi.builtins.AbstractSWRLBuiltInLibrary;
+import org.swrlapi.builtins.SWRLBuiltInLibraryManager;
 import org.swrlapi.builtins.arguments.SWRLBuiltInArgument;
 import org.swrlapi.exceptions.SWRLBuiltInException;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class SWRLBuiltInLibraryImpl extends AbstractSWRLBuiltInLibrary
 {
-  public SWRLBuiltInLibraryImpl() { super("StringsBuiltIns"); }
+  private static final String Namespace = "http://swrl.stanford.edu/ontologies/built-ins/5.2.0/strings.owl#";
+
+  private static final String[] BuiltInNames = { "stringsEqual" };
+
+  static{
+    SWRLBuiltInLibraryManager.registerSWRLBuiltIns(Namespace, BuiltInNames);
+  }
+
+  public SWRLBuiltInLibraryImpl() { super(Namespace, new HashSet<>(Arrays.asList(BuiltInNames))); }
 
   @Override public void reset() { }
 
